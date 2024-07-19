bd3ff9af691d73556f2563c5b3c1819007164ad7
ZakharovDenis <z.d.a.52rus@mail.ru>
2024-07-19 21:29:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1,2,3,'True', False, [4.5]
+print (immutable_var)
+#immutable_var[0]=1.1
+#Кортежи являются неизменяемыми
+# структурами данных, поэтому
+# при попытке их изменить
+# выдаёт ошибку.
+mutable_list=([6,7,8],9)
+print(mutable_list)
